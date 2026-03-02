1020a758f93b52f62f8513b6895298cc805b7592
kuchipudidora-dot <kuchipudidora@gmail.com>
2026-03-02 20:46:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+const http = require("http");
+
+ 
+
+const PORT = 3000;
+
+ 
+
+http.createServer((req, res) => {
+
+  res.write("Hello from Jenkins Docker CI ");
+
+  res.end();
+
+}).listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+
+});
